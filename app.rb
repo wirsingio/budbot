9ba0e079280a1406f5f300fe5c492e4997da31f1
Aaron Cruz <aaron@aaroncruz.com>
2014-08-15 15:25:02 +0200

    Parse incomming stuff

--- app.rb
+++ app.rb
@@ -1,6 +1,34 @@
 require 'json'
 
-post "/run" do
+helpers do
+  def parse_for key, str
+    str.each_line do |line|
+      k, v = line.split("=", 2)
+      if k == key
+        return v.strip
+      end
+    end
+  end
+end
 
-  JSON.dump(text: Time.now.to_s)
+post "/run" do
+  dunno = "I don't understand..."
+  body   = request.body.read
+  puts body
+  puts "#" * 50
+  puts params.inspect
+
+  trigger = params[:trigger_word] || parse_for("trigger_word", body)
+  text = params[:text]            || parse_for("text", body)
+  if text
+    command = text.match(/\A#{trigger}\s*(.*)\z/).captures.first
+    puts text.match(/\A#{trigger}\s*(.*)\z/).captures.first
+    if command
+      if command =~ /time\?/
+        response = Time.now.to_s
+      end
+    end
+  end
+  response ||= dunno
+  JSON.dump(text: response)
 end
