43ced21d2c9faac2a61b83c7098d90bc11cb7406
Aaron Cruz <aaron@aaroncruz.com>
2014-08-15 17:12:59 +0200

    Added cute command

--- app.rb
+++ app.rb
@@ -24,6 +24,7 @@ helpers do
       when "time"    then Responses.time
       when "bud"     then Responses.bud
       when "weather" then Responses.weather
+      when "cute"    then Responses.cute
       else DUNNO
       end
     end
@@ -52,12 +53,29 @@ class Responses
 
     def weather
       url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Wien,at&mode=json&units=metric&cnt=7"
+      data = parse_json_url url
+      days = data[:list]
+      data[:list]
+        .map { |day|
+          Time.at(day[:dt]).strftime("%a") + ": #{day[:temp][:day]}C"
+        }
+        .join("\n")
+    end
+
+    def cute
+      url    = "http://www.reddit.com/r/Aww/hot.json?page=#{1.upto(6).to_a.sample}"
+      data   = parse_json_url(url)
+      data[:data][:children]
+        .map { |image| image[:data][:url] }
+        .select { |link| File.extname(link) == ".jpg" }
+        .sample
+    end
+
+    private
+
+    def parse_json_url url
       body = Net::HTTP.get_response(URI(url)).body
-      days = JSON.parse(body, symbolize_names: true)[:list]
-      days.map { |day|
-        Time.at(day[:dt]).strftime("%a") + ": #{day[:temp][:day]}C"
-      }
-      .join("\n")
+      JSON.parse(body, symbolize_names: true)
     end
   end
 end
