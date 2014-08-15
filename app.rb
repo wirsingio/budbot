06881a3495816b5d880ca062c307b9fa9edefef8
Clemens Helm <clemens.helm@gmail.com>
2014-08-15 19:34:49 +0200

    Fixes in..days thing.

--- app.rb
+++ app.rb
@@ -50,11 +50,12 @@ class Responses
       # only matches cities with one word so far
       city = command.match(/in ([a-zA-Z]+)/).to_a.last || 'Wien'
       in_days = command.match(/in (\d+) days/).to_a.last
-      days_in_query = in_days || 7
+      days_in_query = in_days ? in_days.to_i + 1 : 7
       url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=#{city}&mode=json&units=metric&cnt=#{days_in_query}"
-      STDERR.puts "querying: #{url}"
+      puts url
       data = parse_json_url url
       days = data[:list]
+      puts data[:list]
       days = days.slice(in_days.to_i, 1) if in_days
       days
         .map { |day|
